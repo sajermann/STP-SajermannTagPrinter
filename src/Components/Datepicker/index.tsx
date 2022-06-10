9644554fed50d889a41a215e217c2c66a57db22a
Bruno Sajermann <sajermannbruno@gmail.com>
2022-06-09 22:07:04 -0300

    adjust

--- src/Components/Datepicker/index.tsx
+++ src/Components/Datepicker/index.tsx
@@ -5,28 +5,31 @@ import { useState } from 'react';
 registerLocale('pt-BR', ptBr);
 
 function ExampleCustomTimeInput() {
+	const [startDate, setStartDate] = useState(new Date());
 	return (
-		<input
-			// value={value}
-			onChange={e => console.log(e.target.value)}
-			style={{ border: 'solid 1px pink' }}
-			className="h-24 text-black"
+		<DatePicker
+			selected={startDate}
+			onChange={hour => console.log({ hour })}
+			showTimeSelect
+			showTimeSelectOnly
+			timeIntervals={15}
+			dateFormat="h:mm aa"
 		/>
 	);
 }
 
 export default function Datepicker() {
-	const [startDate, setStartDate] = useState<Date | null>(new Date());
+	const [startDate, setStartDate] = useState<string | undefined>(new Date());
 	return (
 		<DatePicker
 			selected={startDate}
-			onChange={date => setStartDate(date)}
+			onChange={date => console.log(date)}
 			locale="pt-BR"
 			timeFormat="HH:mm"
 			timeInputLabel="Time:"
 			dateFormat="dd/MM/yyyy hh:mm:ss"
 			showTimeInput
-			className="h-24 text-black"
+			className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full grid grid-cols-12 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
 			value={startDate}
 			withPortal
 			customTimeInput={<ExampleCustomTimeInput />}
